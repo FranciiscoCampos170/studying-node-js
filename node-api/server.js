9950b1984881cef69255416e4ce64ceba50566c9
Francisco Campos <franciiscocampos170@gmail.com>
2020-01-30 00:57:22 +0100

    configurando o mongoose e a conexao a base de dados

--- node-api/server.js
+++ node-api/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB      
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+//Primeira Rota
 app.get('/', (req, res) => {
     res.send('Ola Mundo!');
 });
