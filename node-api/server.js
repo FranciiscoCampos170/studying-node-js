26a4f9d9672df51d4346c6c4e6e6a58e96e85ee9
Francisco Campos <franciiscocampos170@gmail.com>
2020-01-31 22:25:37 +0100

    criando registos na base de dados

--- node-api/server.js
+++ node-api/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 //Iniciando o App
 const app = express();
+app.use(express.json());
 
 //Iniciando o DB      
 mongoose.connect('mongodb://localhost:27017/nodeapi', {
